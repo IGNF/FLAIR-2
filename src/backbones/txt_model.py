f92d11b7790fab1928a36302c744529e38ff5466
Anatol Garioud <49435550+agarioud@users.noreply.github.com>
2023-05-08 22:08:44 +0200

    Update txt_model.py

--- src/backbones/txt_model.py
+++ src/backbones/txt_model.py
@@ -71,32 +71,39 @@ class TimeTexture_flair(nn.Module):
             
     def forward(self, config, bpatch, bspatch, dates, metadata):
         
+        ### encoded feature maps and utae outputs
         utae_out , utae_fmaps_dec = self.arch_hr(bspatch, batch_positions=dates)  ### utae class scores and feature maps 
         unet_fmaps_enc = self.arch_vhr.encoder(bpatch)  ### unet feature maps 
         
+        ### aerial metadatat encoding and adding to u-net feature maps
         if config['aerial_metadata'] == True:
             x_enc = self.mtd_mlp(metadata)
             x_enc = x_enc.unsqueeze(1).unsqueeze(-1).repeat(1,unet_fmaps_enc[-1].size()[1],1,unet_fmaps_enc[-1].size()[-1])
             unet_fmaps_enc[-1] = torch.add(unet_fmaps_enc[-1], x_enc) 
-
+        
+        ### cropped fusion module
         transform = T.CenterCrop((10, 10))
         utae_last_fmaps_reshape_cropped = transform(utae_fmaps_dec[-1])    
         utae_last_fmaps_reshape_cropped = self.fm_utae_featmap_cropped(utae_last_fmaps_reshape_cropped, [i.size()[-1] for i in unet_fmaps_enc])       
-       
+        
+        ### collapsed fusion module       
         utae_fmaps_dec_squeezed = torch.mean(utae_fmaps_dec[-1][0], dim=(-2,-1))
         utae_last_fmaps_reshape_collapsed = self.fm_utae_featmap_collapsed(utae_fmaps_dec_squeezed, [i.size()[-1] for i in unet_fmaps_enc])  ### reshape last feature map of utae to match feature maps enc. unet
         
+        ### adding cropped/collasped
         utae_last_fmaps_reshape = [torch.add(i,j) for i,j in zip(utae_last_fmaps_reshape_cropped, utae_last_fmaps_reshape_collapsed)]
 
-
+        ### modality dropout
         if torch.rand(1) > config['drop_utae_modality']:
             unet_utae_fmaps = [torch.add(i,j) for i,j in zip(unet_fmaps_enc, utae_last_fmaps_reshape)]  ### add utae mask to unet feats map
         else:
             unet_utae_fmaps = unet_fmaps_enc
         
+        ### u-net decoding
         unet_out = self.arch_vhr.decoder(*unet_utae_fmaps)  ### unet decoder
         unet_out = self.arch_vhr.segmentation_head(unet_out) ### unet class scores 
         
+        ### reshape utae output to annotation shape
         transform = T.CenterCrop((10, 10))
         utae_last_fmaps_reshape = transform(utae_fmaps_dec[-1])  
         utae_out = self.reshape_utae_output(utae_last_fmaps_reshape)
