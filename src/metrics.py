4004ab5366f10573a55dd2c5a0c80fc0adb8839b
Anatol Garioud <49435550+agarioud@users.noreply.github.com>
2023-05-23 12:16:25 +0200

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -23,20 +23,25 @@ def generate_miou(path_truth: str, path_pred: str) -> list:
                        
     truth_images = sorted(list(get_data_paths(Path(path_truth), 'MSK*.tif')), key=lambda x: int(x.split('_')[-1][:-4]))
     preds_images  = sorted(list(get_data_paths(Path(path_pred), 'PRED*.tif')), key=lambda x: int(x.split('_')[-1][:-4]))
+    
     if len(truth_images) != len(preds_images): 
-        print('[WARNING !] mismatch number of predictions and test files.')
-    if truth_images[0][-10:-4] != preds_images[0][-10:-4] or truth_images[-1][-10:-4] != preds_images[-1][-10:-4]: 
-        print('[WARNING !] unsorted images and masks found ! Please check filenames.') 
-        
-    patch_confusion_matrices = []
+        print('[ERROR !] mismatch number of predictions and test files.')
+        return
+      
+    elif truth_images[0][-10:-4] != preds_images[0][-10:-4] or truth_images[-1][-10:-4] != preds_images[-1][-10:-4]: 
+        print('[ERROR !] unsorted images and masks found ! Please check filenames.') 
+        return
+      
+    else:
+        patch_confusion_matrices = []
 
-    for u in range(len(truth_images)):
-        target = np.array(Image.open(truth_images[u]))-1 # -1 as model predictions start at 0 and turth at 1.
-        target[target>12]=12  ### remapping masks to reduced baseline nomenclature.
-        preds = np.array(Image.open(preds_images[u]))         
-        patch_confusion_matrices.append(confusion_matrix(target.flatten(), preds.flatten(), labels=list(range(13))))
+        for u in range(len(truth_images)):
+            target = np.array(Image.open(truth_images[u]))-1 # -1 as model predictions start at 0 and turth at 1.
+            target[target>12]=12  ### remapping masks to reduced baseline nomenclature.
+            preds = np.array(Image.open(preds_images[u]))         
+            patch_confusion_matrices.append(confusion_matrix(target.flatten(), preds.flatten(), labels=list(range(13))))
 
-    sum_confmat = np.sum(patch_confusion_matrices, axis=0)
-    mIou, ious = calc_miou(sum_confmat) 
+        sum_confmat = np.sum(patch_confusion_matrices, axis=0)
+        mIou, ious = calc_miou(sum_confmat) 
 
-    return mIou, ious
+        return mIou, ious
